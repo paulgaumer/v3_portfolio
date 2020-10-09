ad41de3db79b4aa6c947abc14fdea68a6633091a
Paul Gaumer <mitsudomoe8@gmail.com>
2020-10-09 12:07:05 +0200

    feature: add basic mobile menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,8 @@ const Header = () => {
   const [pathname, setPathname] = useState("/")
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  const handleClick = () => {
+  const handleClick = e => {
+    e.preventDefault()
     setIsMenuOpen(!isMenuOpen)
   }
 
@@ -19,11 +20,11 @@ const Header = () => {
 
   return (
     <header className="pt-4 mb-6 md:pt-6 xl:pt-8">
-      <nav className="flex items-center justify-between pb-4 xl:pb-6 md:border-b md:border-gray-200">
+      <nav className="flex items-center justify-between pb-4 xl:pb-6 sm:border-b sm:border-gray-200">
         <Link to="/">
           <Logo width="w-10" height="h-10" />
         </Link>
-        <ul className="items-center hidden space-x-10 md:flex">
+        <ul className="items-center hidden space-x-10 sm:flex">
           <li>
             <NavLink href="/#portfolio" linkText="portfolio" />
           </li>
@@ -43,8 +44,8 @@ const Header = () => {
             />
           </li>
         </ul>
-        <div className="flex items-center -mr-2 sm:hidden">
-          {/* Mobile menu button */}
+        {/* Mobile menu button Start */}
+        <div className="flex items-center mr-2 sm:hidden">
           <button
             className="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
             aria-label="Main menu"
@@ -52,11 +53,6 @@ const Header = () => {
             onClick={handleClick}
           >
             {/* Icon when menu is closed. */}
-            {/* <!--
-            Heroicon name: menu
-
-            Menu open: "hidden", Menu closed: "block"
-          --> */}
             <svg
               className={`block w-6 h-6 ${isMenuOpen ? "hidden" : "block"}`}
               xmlns="http://www.w3.org/2000/svg"
@@ -88,7 +84,40 @@ const Header = () => {
             </svg>
           </button>
         </div>
+        {/* Mobile menu button Off */}
       </nav>
+      {/* MOBILE MENU BODY START */}
+      <div className={`${isMenuOpen ? "block" : "hidden"} sm:hidden"`}>
+        <div className="pt-2 pb-3 shadow">
+          <Link
+            to="/portfolio"
+            className="block py-2 pl-3 pr-4 text-base font-medium text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300"
+          >
+            Portfolio
+          </Link>
+          <Link
+            to="/about"
+            className="block py-2 pl-3 pr-4 mt-1 text-base font-medium text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300"
+          >
+            About
+          </Link>
+          <Link
+            to="/blog"
+            className="block py-2 pl-3 pr-4 mt-1 text-base font-medium text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300"
+          >
+            Blog
+          </Link>
+          <Link
+            to={`${pathname}${
+              pathname === "/" ? "#contact-form" : "/#contact-form"
+            }`}
+            className="block py-2 pl-3 pr-4 mt-1 text-base font-medium text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+      {/* MOBILE MENU BODY OFF */}
     </header>
   )
 }
