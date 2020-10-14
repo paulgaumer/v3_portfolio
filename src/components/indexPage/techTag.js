2caa6c8761c1bae4f321fe0e182ccc4d874a1c6d
Paul Gaumer <mitsudomoe8@gmail.com>
2020-10-14 11:00:38 +0200

    feature: add tech skills to portfolio section

---
+++ src/components/indexPage/techTag.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react"
+import PropTypes from "prop-types"
+import { GlobalStateContext } from "../../context/contextProvider"
+
+const TechTag = ({ content }) => {
+  const { themeColor, themes } = useContext(GlobalStateContext)
+
+  const getThemeBackground = () => {
+    return themes[themeColor].tag.bg
+  }
+  const getThemeText = () => {
+    return themes[themeColor].tag.text
+  }
+
+  return (
+    <li>
+      <span
+        className={`inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 mt-4 mx-2 ${getThemeBackground()} ${getThemeText()}`}
+      >
+        {content}
+      </span>
+    </li>
+  )
+}
+
+export default TechTag
+
+TechTag.propTypes = {
+  content: PropTypes.string.isRequired,
+}
