0a1adc5ed551e9fa6e3a5a791daa505428cf5887
Paul Gaumer <mitsudomoe8@gmail.com>
2020-10-20 17:28:47 +0200

    feature: convert index page to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,37 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { graphql, PageProps } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/indexPage/heroSection"
 import NavSection from "../components/indexPage/navSection"
 import PortfolioSection from "../components/indexPage/portfolioSection"
 
-const IndexPage = ({ data }) => {
+interface DataProps extends PageProps {
+  data: {
+    allSanityProject: any
+    // allSanityProject: {
+    //   edges: [
+    //     node: {
+    //       id: string,
+    //       title: string,
+    //       url: string,
+    //       _rawBlockDescription: object[],
+    //       frontImage: {
+    //         asset: {
+    //           fluid: object
+    //         }
+    //         hotspot: {
+    //           x:number,
+    //           y:number
+    //         }
+    //       }
+    //     }
+    //   ]
+    // }
+  }
+}
+
+const IndexPage: React.FC = ({data}: DataProps) => {
   const projects = data.allSanityProject.edges.map(({ node }) => node)
 
   return (
@@ -46,7 +71,3 @@ export const query = graphql`
     }
   }
 `
-
-IndexPage.propTypes = {
-  data: PropTypes.object,
-}
